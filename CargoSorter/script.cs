c40e2ddac66b12167bb87cc6c13a6130cdc8f808
Potateau <90016035+Potateau@users.noreply.github.com>
2021-09-06 15:05:11 -0400

    Update script.cs

--- CargoSorter/script.cs
+++ CargoSorter/script.cs
@@ -6,11 +6,15 @@ List<IMyAssembler> allAssemblers = new List<IMyAssembler>();
 List<IMyRefinery> allRefineries = new List<IMyRefinery>();
 //ALL O2/H2 GENGERATORS
 List<IMyGasGenerator> allO2H2Generators = new List<IMyGasGenerator>();
+//ALL GAS TANKS (HYDROGEN AND OXYGEN)
+List<IMyGasTank> allGasTanks = new List<IMyGasTank>();
 
 //temporary inventory
 IMyInventory tempInventory;
 
 MyFixedPoint bottleVolume = (MyFixedPoint)0.120;
+MyFixedPoint amountToMove = (MyFixedPoint)10;
+
 
 public Program()
 {
@@ -24,6 +28,7 @@ public void Main(string argument, UpdateType updateSource)
     CreateAssemblerLists();
     CreateRefineryLists();
     CreateO2H2GeneratorList();
+    CreateGasTankList();
     SortContainers();
 }
 
@@ -56,10 +61,21 @@ public void CreateO2H2GeneratorList()
     //creating the initial O2H2 list of all on the same grid as the programmable block
     GridTerminalSystem.GetBlocksOfType<IMyGasGenerator>(allO2H2Generators, b => b.CubeGrid == Me.CubeGrid);
 }
+public void CreateGasTankList()
+{
+    //clearing the lists
+    allGasTanks.Clear();
+    //creating the initial O2H2 list of all on the same grid as the programmable block
+    GridTerminalSystem.GetBlocksOfType<IMyGasTank>(allGasTanks, b => b.CubeGrid == Me.CubeGrid);
+}
 public void SortContainers()
 {
     //Defining a dumby variable to carry the information to check each inventory slot, reduces required text
     MyInventoryItem tempItem;
+
+    /*
+     Space Engineers will automatically try to sort bottles that need to be refilled into places that they can be refilled, this script will then take them out of there to tool storage
+    */
 
     //sorting all containers
     //all containers should exist in the allCargoContainers list and their individual lists
@@ -76,6 +92,8 @@ public void SortContainers()
         {
             //putting the item in container "i" at slot "x" into the dumby variable tempItem to check where it should go, value function stores everything about the item into tempItem
             tempItem = allCargoContainers[i].GetInventory(0).GetItemAt(x).Value;
+
+
 
             //checking where to place the item
             bool itemInCorrectContainer = false;
@@ -171,6 +189,7 @@ public void SortContainers()
             //we always want to empty out the bottles from the inventories and keep at least one slot open
             //checking to make sure that there is volume available in the generator for a bottle and if not trying to take an item out and put it in the correct container
             //this would be relevant if filled with ice but no need to produce hydrogen or oxygen so the bottles never get refilled and player doesn't have tanks
+
             if(allO2H2Generators[j].GetInventory(0).CurrentVolume>allO2H2Generators[j].GetInventory(0).MaxVolume-bottleVolume)
             {
                 MoveToCorrectCargoContainer(tempItem);
@@ -180,16 +199,29 @@ public void SortContainers()
                 MoveToCorrectCargoContainer(tempItem);
             }
         }
+
     }
+    //for all Oxygen and Hydrogen tanks
+    for (int i = 0; i < allGasTanks.Count(); i++)
+    {
+        //clearing contents in tempCargoContainer so items are not moved accidentally if following assignment fails
+        tempInventory = null;
+        //putting the current container into the temp variable so it can be accessed by other functions
+        tempInventory = allGasTanks[i].GetInventory(0);
+        for (int x= allGasTanks[i].GetInventory(0).ItemCount - 1; x >= 0; x--)
+        {
+            //getting the item in the slot
+            tempItem = allGasTanks[i].GetInventory(0).GetItemAt(x).Value;
+            //we always want to try to empty these tanks to store them with tools if possible
+            MoveToCorrectCargoContainer(tempItem);
+        }
+    }
+
 }
 
 //methods supporting other methods or those that have specific purposes, more general use
 public bool MoveToCorrectCargoContainer(MyInventoryItem itemToMove)
 {
-    /*
-     Space Engineers will automatically try to sort bottles that need to be refilled into places that they can be refilled, this script will then take them out of there to tool storage
-    */
-
     for (int j = 0; j < allCargoContainers.Count; j++)
     {
         //check if the container has been chosen to accept item type and if the container has space to accept the items
@@ -219,6 +251,9 @@ public bool MoveToCorrectCargoContainer(MyInventoryItem itemToMove)
             tempInventory.TransferItemTo(allCargoContainers[j].GetInventory(0), itemToMove);
             return true;
         }
+
+
+
     }
 
     //failed to find a proper container with enough space, move item to misc container if possible
@@ -232,3 +267,4 @@ public bool MoveToCorrectCargoContainer(MyInventoryItem itemToMove)
     }
     return false;
 }
+
